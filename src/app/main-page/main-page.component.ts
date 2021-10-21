fc1772e4ee15385ecfbc3fd49242fb7056d60550
Dmytro Pereklita <74322013+PereDima@users.noreply.github.com>
2021-10-22 02:50:23 +0300

    just coment some code

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -7,30 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent  {
 
-  stars: number[] = [1, 2, 3, 4, 5];
-  selectedValue: number = 0;
+  // stars: number[] = [1, 2, 3, 4, 5];
+  // selectedValue: number = 0;
   
-  countStar(star: any) {
-      this.selectedValue = star;
-    }
+  // countStar(star: any) {
+  //     this.selectedValue = star;
+  //   }
   
-  addClass(star: any) {
-     console.log("star", star); 
-     console.log("selectedvalue", this.selectedValue);
-     let ab = "";
-     for (let i = 0; i < star; i++) {
-       console.log("star i", star);
-       ab = "starId" + i;
-       document.getElementById(ab)!.classList.add("selected");
-     }
-  }
-  removeClass(star: any) {
-     console.log("removestar", star);
-     let ab = "";
-    for (let i = star-1; i >= this.selectedValue; i--) {
-       console.log("star i", star);
-       ab = "starId" + i;
-       document.getElementById(ab)!.classList.remove("selected");
-     }
-  }
+  // addClass(star: any) {
+  //    console.log("star", star); 
+  //    console.log("selectedvalue", this.selectedValue);
+  //    let ab = "";
+  //    for (let i = 0; i < star; i++) {
+  //      console.log("star i", star);
+  //      ab = "starId" + i;
+  //      document.getElementById(ab)!.classList.add("selected");
+  //    }
+  // }
+  // removeClass(star: any) {
+  //    console.log("removestar", star);
+  //    let ab = "";
+  //   for (let i = star-1; i >= this.selectedValue; i--) {
+  //      console.log("star i", star);
+  //      ab = "starId" + i;
+  //      document.getElementById(ab)!.classList.remove("selected");
+  //    }
+  // }
 }
